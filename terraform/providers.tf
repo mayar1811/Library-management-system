aac88809abeaaba3c06be65472391bd27598fb77
mayar1811 <110116265+mayar1811@users.noreply.github.com>
2024-07-31 16:38:22 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,7 +13,6 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.library_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.library_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  config_path            = "~/.kube/config"
-  config_context         = "arn:aws:eks:us-east-1:637423483309:cluster/team6-library-app-cluster"
+
 }
 
