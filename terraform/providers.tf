8abad75db451a4ca2d95db433d4968daef9390af
mayar1811 <110116265+mayar1811@users.noreply.github.com>
2024-08-01 21:28:54 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,6 @@
 provider "aws" {
   region  = var.aws_region
+  profile = var.aws_profile
 }
 
 data "aws_availability_zones" "available" {}
@@ -13,7 +14,5 @@ provider "kubernetes" {
   host                   = aws_eks_cluster.library_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.library_cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  config_path            = "~/.kube/config"
-
+  
 }
-
