607cb16b5b05f7ad786cff2f113911151f1331cf
ahmedhazem99007 <s2051712001eg@std.aou.edu.eg>
2024-08-05 22:01:38 +0300

    Update and rename prometheus-installations.txt to prometheus-installations.sh

--- k8s/prometheus-installations.sh
+++ k8s/prometheus-installations.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+echo "Installing Component for Prometheus"
+
 kubectl apply -f components-prometheus.yaml
---------------------------------------------------------------------------------
+
+
+echo "Add repo to Prometheus"
+
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 --------------------------------------------------------------------------------
+echo "Installing Prometheus using helm "
+
 helm install prometheus \
  prometheus-community/kube-prometheus-stack \
  --namespace monitoring \
  --create-namespace \
  --set alertmanager.persistentVolume.storageClass="gp2",server.persistentVolume.storageClass="gp2"
+
+ echo "Installiton Complete"
+
 
 
